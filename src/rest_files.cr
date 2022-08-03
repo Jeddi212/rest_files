97c90c33ebb6d228d069023ebb6f088e2e533d9e
Jeddi212 <jedediahf51@gmail.com>
2022-08-03 18:32:51 +0700

    add: upload endpoint 🆙

--- src/rest_files.cr
+++ src/rest_files.cr
@@ -1,6 +1,24 @@
-# TODO: Write documentation for `RestFiles`
-module RestFiles
-  VERSION = "0.1.0"
+require "kemal"
 
-  # TODO: Put your code here
+get "/" do
+  "Hello World!"
 end
+
+post "/upload" do |env|
+  HTTP::FormData.parse(env.request) do |upload|
+    filename = upload.filename
+    
+    if !filename.is_a?(String)
+    # Be sure to check if file.filename is not empty otherwise it'll raise a compile time error
+      p "No filename included in upload"
+    else
+      file_path = File.join [Kemal.config.public_folder, "uploads/", filename]
+      File.open(file_path, "w") do |f|
+        IO.copy(upload.body, f)
+      end
+      "Upload ok"
+    end
+  end
+end
+
+Kemal.run
